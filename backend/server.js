07676a2ba06a4398c67b1e897d37c11ff41df051
Md Rafeeq <97541181+md-rafiq-03@users.noreply.github.com>
2024-02-24 08:54:41 +0000

    frontend searchbar

--- backend/server.js
+++ backend/server.js
@@ -63,10 +63,11 @@ pool.getConnection((err, connection) => {
     });
     
 
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 4000;
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 
     connection.release();
 });
+ 
