29b6d5d247213307b331c964c442215ccc55380a
Md Rafeeq <97541181+md-rafiq-03@users.noreply.github.com>
2024-02-24 07:24:46 +0000

    table and search bar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,12 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { WeekDataComponent } from './week-data/week-data.component';
 import { FooterComponent } from './footer/footer.component';
-import { UserDataComponent } from './user-data/user-data.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent,WeekDataComponent,UserDataComponent,FooterComponent],
+  imports: [RouterOutlet,NavBarComponent,WeekDataComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
